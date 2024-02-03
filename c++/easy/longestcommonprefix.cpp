524eb923ccc531a87e65398a8fe86d7d2dacec12
Deathstroke Lowell <deathstl@mi-w6.msk.21-school.ru>
2024-02-03 17:13:48 +0300

    feat: add task merge sorted list

--- c++/easy/longestcommonprefix.cpp
+++ c++/easy/longestcommonprefix.cpp
@@ -3,41 +3,41 @@
 #include <vector>
 #include <algorithm>
 
-// std::string longestCommonPrefix(std::vector<std::string>& v) {
-//     std::string ans="";
-//     std::sort(v.begin(),v.end());
-//     int n=v.size();
-//     std::string first=v[0],last=v[n-1];
-//     for(int i=0;i<std::min(first.size(),last.size());i++){
-//         if(first[i]!=last[i]){
-//             return ans;
-//         }
-//         ans+=first[i];
-//     }
-//     return ans;
-// }
-
-std::string longestCommonPrefix(std::vector<std::string>& strs) {
-    if (strs.empty()) {
-        return "";
-    }
-
-    // Перебираем символы в первой строке
-    for (int i = 0; i < strs[0].size(); ++i) {
-        char currentChar = strs[0][i];
-
-        // Проверяем, содержится ли текущий символ в остальных строках на той же позиции
-        for (int j = 1; j < strs.size(); ++j) {
-            if (i >= strs[j].size() || strs[j][i] != currentChar) {
-                // Если текущий символ не совпадает во всех строках, возвращаем префикс до этой позиции
-                return strs[0].substr(0, i);
-            }
+std::string longestCommonPrefix(std::vector<std::string>& v) {
+    std::string ans="";
+    std::sort(v.begin(),v.end());
+    int n=v.size();
+    std::string first=v[0],last=v[n-1];
+    for(int i=0;i<std::min(first.size(),last.size());i++){
+        if(first[i]!=last[i]){
+            return ans;
         }
+        ans+=first[i];
     }
-
-    // Если мы дошли до этой точки, то весь первый строка - общий префикс
-    return strs[0];
+    return ans;
 }
+
+// std::string longestCommonPrefix(std::vector<std::string>& strs) {
+//     if (strs.empty()) {
+//         return "";
+//     }
+
+//     // Перебираем символы в первой строке
+//     for (int i = 0; i < strs[0].size(); ++i) {
+//         char currentChar = strs[0][i];
+
+//         // Проверяем, содержится ли текущий символ в остальных строках на той же позиции
+//         for (int j = 1; j < strs.size(); ++j) {
+//             if (i >= strs[j].size() || strs[j][i] != currentChar) {
+//                 // Если текущий символ не совпадает во всех строках, возвращаем префикс до этой позиции
+//                 return strs[0].substr(0, i);
+//             }
+//         }
+//     }
+
+//     // Если мы дошли до этой точки, то весь первый строка - общий префикс
+//     return strs[0];
+// }
 
 int main() {
     // std::vector<std::string> v = {"tanga", "fanta", "sange"};
@@ -46,4 +46,3 @@ int main() {
     std::cout<<longestCommonPrefix(v)<<std::endl;
     return 0;
 }
-
---
+++ c++/easy/mergesortedlist.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    // Create a dummy node to simplify the code
+    ListNode* dummy = new ListNode(0);
+    ListNode* current = dummy;
+
+    while (l1 != nullptr && l2 != nullptr) {
+        if (l1->val < l2->val) {
+            current->next = l1;
+            l1 = l1->next;
+        } else {
+            current->next = l2;
+            l2 = l2->next;
+        }
+        current = current->next;
+    }
+
+    // If one of the lists is not empty, append it to the merged list
+    if (l1 != nullptr) {
+        current->next = l1;
+    } else {
+        current->next = l2;
+    }
+
+    // Save the head of the merged list and delete the dummy node
+    ListNode* mergedList = dummy->next;
+    delete dummy;
+
+    return mergedList;
+}
+
+// Utility function to print the linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        std::cout << head->val << " ";
+        head = head->next;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    // Example 1
+    ListNode* list1 = new ListNode(1);
+    list1->next = new ListNode(2);
+    list1->next->next = new ListNode(4);
+
+    ListNode* list2 = new ListNode(1);
+    list2->next = new ListNode(3);
+    list2->next->next = new ListNode(4);
+
+    ListNode* mergedList1 = mergeTwoLists(list1, list2);
+    printList(mergedList1);
+
+    // Example 2
+    ListNode* list3 = nullptr;
+    ListNode* list4 = nullptr;
+
+    ListNode* mergedList2 = mergeTwoLists(list3, list4);
+    printList(mergedList2);
+
+    // Example 3
+    ListNode* list5 = nullptr;
+    ListNode* list6 = new ListNode(0);
+
+    ListNode* mergedList3 = mergeTwoLists(list5, list6);
+    printList(mergedList3);
+
+    return 0;
+}
